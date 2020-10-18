a6e5a7b98fbadef4a9b5d1342e1c730af806a641
Suzanne Cabral <suzannecabral@live.com>
2020-10-18 13:46:35 -0700

    Initial commmit, added styles

---
+++ pr3-reduxapi-poke-move-finder/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="accentBar">
+        &nbsp;
+      </div>
+      <header>
+        <h1>Pokemon Move Finder</h1>
+      </header>
+      <div className="contentWrapper">
+      </div>
+    </div>
+  );
+}
+
+export default App;
